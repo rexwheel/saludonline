5f281576101ca4eefb086e019c2a8e353850f1cd
Nathanael Espinal <n.espinal2@caribemedia.com.do>
2021-02-10 16:42:40 -0400

    Montando la vaina

---
+++ router/src/components/Perfil.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="perfil">
+        <h1>Perfil del cliente desde componente: {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Cliente'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ router/src/views/Clientes.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="clientes">
+        <h1>Cliente desde el view: </h1>
+        
+        <router-link to="{ name:'Clientes', params:{id: item} }" 
+        v-for="(item,index) of perfiles" :key="index">
+            <button>
+                Foto {{ item }}
+            </button>
+        </router-link>
+
+
+
+
+
+        <Perfil />
+
+        <!-- moverse por ruta con JS -->
+        <button @click="Inicio">Inicio</button>
+        <button @click="Anterior">Anterior</button>
+        <button @click="Siguiente">Siguiente</button>
+
+
+
+
+
+
+    </div>
+</template>
+
+
+<script>
+import Perfil from "@/components/Perfil.vue";
+export default {
+    name: 'Perfiles',
+    components: {
+        Perfil
+    },
+
+    data(){
+        return{
+            perfiles: [1,2,3]
+        }
+    },
+
+    methods:{
+        Inicio(){
+            this.$router.push('/')
+        },
+        
+        Anterior(){
+            this.$router.go(-1)
+        },
+
+        Siguiente(){
+            this.$router.go(1)
+        }
+    }
+}
+</script>
